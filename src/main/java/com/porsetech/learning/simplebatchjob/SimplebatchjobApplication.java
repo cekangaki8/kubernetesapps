03214d86342237e5d3af715e621c7ef99753c381
Candy Ekangaki <cekangaki8@gmail.com>
2022-09-11 15:36:19 -0400

    Initial project setup

---
+++ src/main/java/com/porsetech/learning/simplebatchjob/SimplebatchjobApplication.java
@@ -0,0 +1,13 @@
+package com.porsetech.learning.simplebatchjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebatchjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplebatchjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/porsetech/learning/simplebatchjob/SimplebatchjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porsetech.learning.simplebatchjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebatchjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
